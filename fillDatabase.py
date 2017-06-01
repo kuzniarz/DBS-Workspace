859a12ac362657f1c04ed7a332e07effd45bcd2a
Kuzniarz <indirectacoustic@gmx.de>
2017-06-02 01:09:03 +0200

    Update fillDatabase.py

--- fillDatabase.py
+++ fillDatabase.py
@@ -1,18 +1,18 @@
 import csv
 import psycopg2
 
+#conn_string contains all the needed settings for the database connection
 conn_string = "host='localhost' dbname='election' user='postgres' password='postgres'"
 
 conn = psycopg2.connect(conn_string)
-
 cursor = conn.cursor()
 
+#Read Tweet Data and copy to database
 inputT= open(r'/home/kuzniarz/Desktop/Workspace/Datenbanken/Projekt/DBS-Workspace/aetDATA.csv', 'r')
 cursor.copy_from(inputT, "tweet", sep=';')
 inputT.close()
 
+#Read Hashtag Data and copy to database
 inputH= open(r'/home/kuzniarz/Desktop/Workspace/Datenbanken/Projekt/DBS-Workspace/aetTAG.csv', 'r')
 cursor.copy_from(inputH, "hashtag", sep=';')
 inputH.close()
-
-#an der stelle muss nun ein cursor.execute kommen welcher die Relationelle Tabelle erstellt. Damit das so hinhaut muss man Aufgabe 2 noch anpassen(spalten aus tweet liste richtig sortieren und hashtag liste mit total_count versehen), sonst müsste das Ding komplett richtig sein
